d51eee2bba6c878553697f8263a2d4f324f1702a
Joshua Clark <joshuaclark94@gmail.com>
2015-05-01 20:30:18 +0100

    Remove unneeded field, and ensure get method matches specification

--- src/model/PGate.scala
+++ src/model/PGate.scala
@@ -6,14 +6,9 @@ import helper.Direction
  * P(g, s, d) = ¬g -> (s <-> d)
  *
  * P gates can only carry high potential from the source to their drain
- * @param input
- * @param source
- * @param drain
  */
-case class PGate(input : Node, source : Wire, drain : Wire) extends Gate {
-  var placement : Option[Direction] = None
-
-  def get = if (input.get) source.get() else Undriven()
+case class PGate(input : Node, drain : Wire, source : Wire) extends Gate {
+  override def get : Potential = if (!input.get) source.get() else Undriven()
 
   def drainDriven = input.get && (source.get == High())
 }
