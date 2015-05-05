2ae9fb2f1b10787f4733baa5fb4818774761853d
Joshua Clark <joshuaclark94@gmail.com>
2015-05-05 17:31:44 +0100

    Drawing changes to display files for gates rather than rectangles, and to add top and bottom wires

--- src/view/DrawCircuit.scala
+++ src/view/DrawCircuit.scala
@@ -7,7 +7,7 @@ import model._
 /**
  * Created by joshua on 08/04/15.
  */
-case class DrawCircuit(val graph : mxGraph) {
+case class DrawCircuit (val graph : mxGraph) {
   val nodes = scala.collection.mutable.Stack[AnyRef]()
   val deltaY = 50
   val deltaX = 25
@@ -19,30 +19,38 @@ case class DrawCircuit(val graph : mxGraph) {
   var drainsYLimit = 0;
   var maxX = 0;
 
-  def draw() : Unit = {
+  def draw () : Unit = {
     parent = graph.getDefaultParent
+    styleSheet("nmos_de")
+    styleSheet("pmos_de")
+    styleSheet("nmos_en")
+    styleSheet("pmos_en")
     graph.getModel.beginUpdate()
-    try {
+    try { {
       val node = graph.insertVertex(parent, null, "output", 0, 0, 0, 1)
       val sources = Result.getSources
       // every gate in sources should connect to the output wire
-      for (sources <- sources) nodes push node
+      for (sources <- sources) {
+        nodes push node
+      }
       currentX = 0
       drawNetwork(sources, 0, true)
       maxX = currentX
       currentX = -10
       val drains = Result.getDrains
-      for (drain <- drains) nodes push node
+      for (drain <- drains) {
+        nodes push node
+      }
       drawNetwork(drains, 0, false)
       maxX = Math.max(maxX, currentX)
       val newOut = graph.insertVertex(parent, null, "output", -15, 0, maxX + 30, 1, mxConstants.STYLE_SHAPE + "=" +
-        mxConstants.SHAPE_LINE)
+                                                                                    mxConstants.SHAPE_LINE)
 
-      val drain = graph.insertVertex(parent, null, "drain", -15, drainsYLimit + 2*deltaY, maxX + 30, 1, mxConstants
-        .STYLE_SHAPE + "=" + mxConstants.SHAPE_LINE)
+      val drain = graph.insertVertex(parent, null, "drain", -15, drainsYLimit + 2 * deltaY, maxX + 30, 1,
+                                     mxConstants.STYLE_SHAPE + "=" + mxConstants.SHAPE_LINE)
 
-      val source = graph.insertVertex(parent, null, "source", -15, -1.0 * (sourcesYLimit + deltaY), maxX + 30, 1, mxConstants
-        .STYLE_SHAPE + "=" + mxConstants.SHAPE_LINE)
+      val source = graph.insertVertex(parent, null, "source", -15, -1.0 * (sourcesYLimit + deltaY), maxX + 30, 1,
+                                      mxConstants.STYLE_SHAPE + "=" + mxConstants.SHAPE_LINE)
 
 
       if (Result.get() != Undriven()) {
@@ -55,23 +63,45 @@ case class DrawCircuit(val graph : mxGraph) {
       }
 
       for (sourceGate <- lastSources) {
-        graph.insertEdge(parent, null, "", sourceGate, source)
+        val x = graph.getCellGeometry(sourceGate).getCenterX
+        val dot = graph.insertVertex(parent, null, "", x, -1.0 * (sourcesYLimit + deltaY), 0, 0)
+        graph.insertEdge(parent, null, "", sourceGate, dot)
       }
 
       for (drainGate <- lastDrains) {
-        graph.insertEdge(parent, null, "", drainGate, drain)
+        val x = graph.getCellGeometry(drainGate).getCenterX
+        val dot = graph.insertVertex(parent, null, "", x, drainsYLimit + deltaY, 0, 0)
+        graph.insertEdge(parent, null, "", drainGate, dot)
       }
 
       lastSources.clear()
       lastDrains.clear()
 
       graph.removeCells(Array(node))
+    }
     } finally {
       graph.getModel.endUpdate()
     }
   }
 
-  private[this] def drawNetwork(gates : Array[Gate], y : Int, drawingTopNetwork : Boolean) : Unit = {
+  private def styleSheet (name : String) : Unit = {
+    // add gate as node at (xPos, y+1)
+    // get graph stylesheet
+    val stylesheet = graph.getStylesheet
+
+    // define image style name
+    val styleName = name
+
+    // define image style
+    val style = new java.util.HashMap[String, AnyRef]()
+    style.put(mxConstants.STYLE_SHAPE, mxConstants.SHAPE_IMAGE)
+    style.put(mxConstants.STYLE_IMAGE, "file:resources/%s.png".format(name))
+    style.put(mxConstants.STYLE_LABEL_POSITION, mxConstants.ALIGN_LEFT)
+
+    stylesheet.putCellStyle(styleName, style)
+  }
+
+  private[this] def drawNetwork (gates : Array[Gate], y : Int, drawingTopNetwork : Boolean) : Unit = {
     val yPos = y
     if (gates.isEmpty) {
       // if last node on the chain, nothing to connect it to, so forget it
@@ -82,35 +112,34 @@ case class DrawCircuit(val graph : mxGraph) {
         lastDrains += nodes.pop()
         sourcesYLimit = scala.math.max(drainsYLimit, y)
       }
-    } else for (gate <- gates) {
-      // add gate as node at (xPos, y+1)
-      val previousNode = nodes.pop()
-      val node =
-        graph.insertVertex(parent, null, gate.input.toString +
-          (if (drawingTopNetwork) {
-            "sources"
-          } else {
-            "drains"
-          }), currentX,
-          if (!drawingTopNetwork) {
+    } else {
+      for (gate <- gates) {
+        val previousNode = nodes.pop()
+        val node =
+          graph.insertVertex(parent, null, gate.input.toString, currentX, if (!drawingTopNetwork) {
             y
           } else {
             -(y + deltaY)
-          }, deltaX, deltaY)
-      graph insertEdge(parent, null, "", previousNode, node, mxConstants.STYLE_SHAPE + "=" + mxConstants
-        .SHAPE_IMAGE)
+          }, deltaX * 2.0, deltaY, (if (!drawingTopNetwork) {
+            "nmos"
+          } else {
+            "pmos"
+          }) + (if (gate.get() == Undriven()) {
+            "_de"
+          } else {
+            "_en"
+          }))
+        graph insertEdge(parent, null, "", previousNode, node, mxConstants.STYLE_SHAPE + "=" + mxConstants.SHAPE_IMAGE)
 
-      nodes push node
-      if (gate.get() != Undriven()) {
-        graph.setCellStyle("fillColor=green", Array(node))
+        nodes push node
+
+        drawNetwork(if (drawingTopNetwork) {
+          gate.source.getSources
+        } else {
+          gate.drain.getDrains
+        }, yPos + deltaY, drawingTopNetwork)
+        currentX += deltaX * 2
       }
-
-      drawNetwork(if (drawingTopNetwork) {
-        gate.source.getSources
-      } else {
-        gate.drain.getDrains
-      }, yPos + deltaY, drawingTopNetwork)
-      currentX += deltaX * 2
     }
   }
 }
